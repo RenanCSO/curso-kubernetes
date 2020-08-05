80c596f5cc59a9e94753b49f093066c56605a5f7
RenanCSO <renan.c.s.o@gmail.com>
2020-08-05 17:27:11 -0300

    Config lifecycle para enviar mensagens no Slack

---
+++ sistema/enviarMensagens.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Olá, um novo POD foi criado!"}' https://hooks.slack.com/services/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
